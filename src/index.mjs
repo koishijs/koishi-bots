6781b72d47fdb31726a573587336f4e22a184829
yijie4188 <yijie4188@gmail.com>
2021-08-03 17:06:08 +0800

    feat: import database and mark plugin

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import { App } from 'koishi'
 
 import 'koishi-adapter-onebot'
 
+import dotenv from './dotenv.mjs'
 import getCqBots from './getCqBots.mjs'
 
 const app = new App({
@@ -10,5 +11,11 @@ const app = new App({
     // 添加除了 onebot 协议以外的 bot 配置
   ]))
 })
+
+import * as mongo from 'koishi-plugin-mongo'
+import * as mark from 'koishi-plugin-mark'
+
+app.plugin(mongo, dotenv().database)
+app.plugin(mark)
 
 app.start().then(_ => {})
