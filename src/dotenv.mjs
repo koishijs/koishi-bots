c929f0a8de14c84d87d8a352529c4af7e9e1ec70
yijie4188 <yijie4188@gmail.com>
2021-08-03 16:54:53 +0800

    feat: load config from .env file

---
+++ src/dotenv.mjs
@@ -0,0 +1,25 @@
+import yaml from 'js-yaml'
+import fs from 'fs'
+import path from 'path'
+
+/** @typedef {{
+  database: {
+    type: 'mongodb' | 'mysql' | string,
+    host: string,
+    port: number,
+    username: string,
+    password: string,
+    name: string
+  }
+}} EnvConfig */
+
+/**
+ * 获取当前环境配置数据
+ *
+ * @param {string} [env=process.env.NODE_ENV] - 当前运行环境，默认使用环境变量中的 NODE_ENV 变量，变量不存在时使用 'dev'
+ * @return {EnvConfig}
+ */
+export default (env) => {
+  !env && (env = process.env.NODE_ENV || 'dev')
+  return /** @type {EnvConfig} */ yaml.load(fs.readFileSync(path.resolve(process.cwd(), `.env/.${env}.yml`)))
+}
