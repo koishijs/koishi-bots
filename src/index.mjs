d688db0a7758c3aecc48cb70d9f0532b8b66a7a2
yijie4188 <yijie4188@gmail.com>
2021-08-04 22:59:32 +0800

    refactor: rollup back to second-jie bot

--- src/index.mjs
+++ src/index.mjs
@@ -8,7 +8,7 @@ import getCqBots from './getCqBots.mjs'
 
 const app = new App({
   port: 8080,
-  bots: getCqBots(['third-jie']).concat(/** @type { (import('koishi').BotOptions)[] } */ ([
+  bots: getCqBots(['second-jie']).concat(/** @type { (import('koishi').BotOptions)[] } */ ([
     // 添加除了 onebot 协议以外的 bot 配置
   ]))
 })
